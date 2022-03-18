d03312f2651b3010376e878d22043d915afb3f10
politopilot <tathomas@student.ethz.ch>
2022-03-18 10:19:21 +0100

    something

--- lab02/mP/hall_daq.c
+++ lab02/mP/hall_daq.c
@@ -57,6 +57,7 @@ int main()
     }
 
 	// Convert the voltage value to magnetic field
+    
 
 
     // Close the serial port
--- lab02/mP/hall_sensor.c
+++ lab02/mP/hall_sensor.c
@@ -2,6 +2,15 @@
 #include "hall_sensor.h"
 //Function hall_sensor_get_field converts the measured voltage value to a magnetic field (in milli-tesla)
 float hall_sensor_get_field(float voltage, float voltage_0)
-{
-   // Insert your code here
+{  
+   float D = 0.01; //in m
+   float R = 0.02; //in m
+
+   // convert voltage into distance z
+
+
+
+
+
+   // return field
 }
