bdf869ca2b1b3a9db60457fa718cc32300d9a1e3
politopilot <tathomas@student.ethz.ch>
2022-03-21 19:19:44 +0100

    sui

--- lab02/mP/hall_daq.c
+++ lab02/mP/hall_daq.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <unistd.h>
 #include "feather_serial.h"
+#include "hall_sensor.h"
 
 // include your function definitions here
 
@@ -21,7 +22,7 @@ int main()
     // initialize the serial port on the port /dev/ttyUSB0, with baud rate 115200
     int fd = serialport_init( "/dev/ttyUSB0", 115200);
 
-
+/*
     for(int i = 0; i < 50; i++){
 
         // write to the serial port to get a value
@@ -30,39 +31,62 @@ int main()
         if(n == -1 )
             printf("Unable to write the port\n");
         
-        // Let the user know if you were able to write to the port
-
-
-
         // Read the sensor value into the buffer from the serial port
         n = serialport_read(fd, buf, 4, 500);
         if(n == -1 )
             printf("Unable to read the port \n");
-        
-        // Let the user know if you were able to read from the port
-
 
         // Convert the sensor value to a voltage
         pin_reading = atoi(buf); // cast to integer
         volt =  3.3 * (float)pin_reading / 4095;
 
-        /*for(int u = 0; u < 4; u++){
-            printf("%d ", buf[u]);
-        }*/
+        printf("%f\n", volt);      
+        // Convert the voltage value to magnetic field
+        float B = hall_sensor_get_field(volt, quis_volt);
+        printf("%f mT\n",B);
 
-        //printf("pinreading: %d\n",pin_reading);
-        //printf("volt:%f\n", volt);
+    }*/
 
-        printf("%f\n", volt);
-        
-    }
+    char user_input = '1';
+    int d = 40;
+     int step_size = 2;
 
-	// Convert the voltage value to magnetic field
+   do {
+        user_input = getchar();
+        if (user_input != '\n' && user_input != '0') {
 
 
 
-    // Close the serial port
+
+
+    // write to the serial port to get a value
+    char arg = '0';
+    n = serialport_write(fd,((char*)&arg));
+    if(n == -1 )
+        printf("Unable to write the port\n");
+    
+    // Read the sensor value into the buffer from the serial port
+    n = serialport_read(fd, buf, 4, 500);
+    if(n == -1 )
+        printf("Unable to read the port \n");
+
+    // Convert the sensor value to a voltage
+    pin_reading = atoi(buf); // cast to integer
+    volt =  3.3 * (float)pin_reading / 4095;
+
+    //printf("%f\n", volt);      
+    // Convert the voltage value to magnetic field
+    float B = hall_sensor_get_field(volt, quis_volt);
+    printf("%d\t",d);
+    printf("%f\n",B); //mT
+    fprintf( stderr, "%d\t%f\n", d, B);
+
+    d -= step_size;
+      }
+}
+while (user_input != '0');
+// Close the serial port
     serialport_close(fd);
-	return 0;
+    return 0;
 
 }
--- lab02/mP/hall_sensor.c
+++ lab02/mP/hall_sensor.c
@@ -4,31 +4,10 @@
 //Function hall_sensor_get_field converts the measured voltage value to a magnetic field (in milli-tesla)
 float hall_sensor_get_field(float voltage, float voltage_0)
 {  
-   float D = 0.01; //in m
-   float R = 0.02; //in m
-   float n = 0.1;
-   float B_r = 175.6;
    float B;
+   float sens = 0.05; // V/mT
 
-   // convert voltage into distance z
-   float z;
-   // 1/0.04m^n with mean voltage 1.6872V
-   // 1/0.02m^n with mean voltage 2.3232V
-
-   // Voltage = 1/z^n * m + voltage_0 
-   // m = (2.3232-1.6872)/(1/0.02m^n - 1/0.04m^n)
-   // slope m = 6.4224 [V*z^n]
-   float m = 6.4224;
-
-   // formula for z:
-
-   z = pow((m/(voltage-voltage_0)),1/n); // in m
-
-   float R2 = pow(R,2);
-   float z2 = pow(z,2);
-   float Dz2 = pow(D+z,2);
-
-   B = (B_r/2) * ((D+z)/pow(R2 + Dz2, 0.5) - z/pow(R2 + z2,0.5));
+   B = (voltage-voltage_0)/sens;
 
    return B;
 }
